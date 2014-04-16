f12f78055d87dbb61585f5aa510a7529464858c1
Waihon Yew <yewwaihon@gmail.com>
2014-04-17 00:31:45 +0800

    Initial commit

---
+++ app/models/listing.rb
@@ -0,0 +1,6 @@
+class Listing < ActiveRecord::Base
+	# Please note no > after 200x, and > after 100x100 
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+	# To resolve Paperclip::Errors::MissingRequiredValidatorError 
+	validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+end
