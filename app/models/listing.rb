6db97eb368a349f89b197ff80f0c195b4cce29c7
Waihon Yew <yewwaihon@gmail.com>
2014-04-18 13:22:09 +0800

    Finished set up for Dropbox

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,14 @@
 class Listing < ActiveRecord::Base
 	# Please note no > after 200x, and > after 100x100 
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
-		:storage => :dropbox,
-		:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg"
+	else
+		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+			:storage => :dropbox,
+			:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+			:path => ":style/:id_:filename"
+	end
+
 	# To resolve Paperclip::Errors::MissingRequiredValidatorError 
 	validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
 end
