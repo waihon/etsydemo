e1dfcfb30cf48b00a31aba0d760b91136a4afa2f
Waihon Yew <yewwaihon@gmail.com>
2014-04-25 01:00:52 +0800

    Add name field to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :user
-    devise_parameter_sanitizer.for(:account_update) << :user
+    devise_parameter_sanitizer.for(:sign_up) << :name
+    devise_parameter_sanitizer.for(:account_update) << :name
   end
 end
