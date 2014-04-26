5d4fc09fea305222fee9979c40076419c5ced57c
Waihon Yew <yewwaihon@gmail.com>
2014-04-26 10:47:26 +0800

    Associate listings with users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140425005954) do
+ActiveRecord::Schema.define(version: 20140426014302) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
