f8cedb41111af7740a8982e81587ef9b0b8e0079
Waihon Yew <yewwaihon@gmail.com>
2014-04-29 13:19:09 +0800

    Charge credit cards

--- app/assets/javascripts/orders.js.coffee
+++ app/assets/javascripts/orders.js.coffee
@@ -11,7 +11,12 @@ payment =
 
   handleStripeResponse: (status, response) ->
     if status == 200
-      alert(response.id)
+      # When the credit card info is valid, add the Stripe token as a hidden field.
+      #alert(response.id)
+      $('#new_order').append($('<input type="hidden" name="stripeToken" />').val(response.id))
     else
-      alert(response.error.message)
+      # Display the Stripe error message in the '<div id="stripe_error"...' section of _form.html.erb in orders.
+      $('#stripe_error').text(response.error.message).show()
+      # Re-enable the 'Complete Order ' button
+      $('input[type=submit]').attr('disabled', false)
 	
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,10 +36,29 @@ class OrdersController < ApplicationController
     @seller = @listing.user
     @order.seller_id = @seller.id
 
+    # Charge the credit card.
+    # The stripeToken was set in orders.js.coffee script
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+    token = params[:stripeToken]
+
+    begin
+      charge = Stripe::Charge.create(
+        # Convert dollars to cents and round down to integer
+        :amount => (@listing.price * 100).floor
+        :currency => "usd"
+        :card => token
+        )
+      flash[:notice] = "Thanks for ordering!"
+    rescue Stripe::CardError => e
+      flash[:danger] = e.message
+    end
+
     respond_to do |format|
       if @order.save
         # Redirect to root url instead of order page 
-        format.html { redirect_to root_url, notice: 'Order was successfully created.' }
+        #format.html { redirect_to root_url, notice: 'Order was successfully created.' }
+        # Flash message was taken care above based on whether it's successful or otherwise. 
+        format.html { redirect_to root_url }
         format.json { render :show, status: :created, location: @order }
       else
         format.html { render :new }
